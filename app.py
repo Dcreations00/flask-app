d63968119fbe093b54cd2836f38e8694c9d8af57
Dcreations00 <dhanushka1508@gmail.com>
2024-06-26 17:03:34 +0530

    v1

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    # Do anything in this function and then return the final value to be displayed on the browser
+    return "<h1>Hello world</h1>"
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
+    

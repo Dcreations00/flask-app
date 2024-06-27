7f69b3750b59494ecc529944b86219cff1c03432
Dcreations00 <87937529+Dcreations00@users.noreply.github.com>
2024-06-27 11:24:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     # Do anything in this function and then return the final value to be displayed on the browser
-    return "<h1>Hello world</h1>"
+    return "<h1>Hello worlddsfdhg</h1>"
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
